363e2941f8af5efca348828d6cafe76eaecedf25
Chen Xu <windoze.net@gmail.com>
2017-01-30 21:42:22 +0800

    Coroutine and CoroutineSequence

--- Sources/Coroutine.swift
+++ Sources/Coroutine.swift
@@ -8,60 +8,63 @@
 import Context
 
 // HACK: Type eraser
-protocol Startable : AnyObject {
+
+protocol Startable: AnyObject {
     func start()
 }
 
-/**
+xzswazZZ/**
  * Coroutine iterator, support for loop
  */
-public struct CoroutineIterator<Element> : IteratorProtocol {
-    var owner: Coroutine<Element>
-    init(owner: Coroutine<Element>) {
-        self.owner=owner
+
+public struct CoroutineIterator<Element>: IteratorProtocol {
+    var owner: CoroutineSequence<Element>
+    init(owner: CoroutineSequence<Element>) {
+        self.owner = owner
     }
-    public func next()->Element? {
+
+    public func next() -> Element? {
         return owner.next()
     }
 }
 
 /**
- * Coroutine
+ * CoroutineSequence
  */
-public class Coroutine<UpType>: Startable, Sequence {
-    public typealias Element = UpType
+
+public class CoroutineSequence<Element>: Startable, Sequence {
     public typealias Iterator = CoroutineIterator<Element>
-    
+
     var stack: coro_stack
     var caller_ctx: coro_context
     var coro_ctx: coro_context
-    var entry: (_:(UpType)->()) -> ()
-    var completed:Bool
-    var upValue: UpType?
-    
+    var entry: (_: (Element) -> Void) -> Void
+    var completed: Bool
+    var upValue: Element?
+
     func start() {
         // Now we're in callee context
-        self.entry({ (up:UpType)->() in
-            self.upValue=up
+        self.entry({ (up: Element) -> Void in
+            self.upValue = up
             coro_transfer(&self.coro_ctx, &self.caller_ctx)
         })
-        completed=true
+        completed = true
         // Switch back to caller
         coro_transfer(&self.coro_ctx, &self.caller_ctx)
     }
-    
+
     /**
      * Called from caller context, continues the coroutine
      * Returns an upValue set by yield called by coroutine
      */
-    public func next()->UpType? {
-        if(!completed) {
+    public func next() -> Element? {
+        if (!completed) {
             self.upValue = Optional.none
             coro_transfer(&self.caller_ctx, &self.coro_ctx)
         }
         return self.upValue
     }
-    
+
     /**
      * Conform to Sequence protocol 
      */
@@ -73,22 +76,83 @@ public class Coroutine<UpType>: Startable, Sequence {
      * Create a coroutine, supply a callback function acts as "yield"
      * Coroutine calls the callback to yield an upValue
      */
-    public init(entry:@escaping (_:(UpType)->())->Void, withSuggestedStackSize:UInt32 = 0) {
-        stack=coro_stack()
+    public init(entry: @escaping (_: (Element) -> Void) -> Void, withSuggestedStackSize: UInt32 = 0) {
+        stack = coro_stack()
         coro_stack_alloc(&stack, withSuggestedStackSize)
-        caller_ctx=coro_context()
-        coro_ctx=coro_context()
-        self.entry=entry
-        completed=false
+        caller_ctx = coro_context()
+        coro_ctx = coro_context()
+        self.entry = entry
+        completed = false
         coro_create(&coro_ctx,
-                    { (ptr:UnsafeMutableRawPointer?) -> () in
-                        // HACK: Swift doesn't allow C routine calls back to a generic closure
-                        // Use protocol Startable as a type eraser
-                        (Unmanaged<AnyObject>.fromOpaque(ptr!).takeRetainedValue() as! Startable).start()
-                    },
-                    Unmanaged.passUnretained(self).toOpaque(), stack.sptr, stack.ssze)
+                { (ptr: UnsafeMutableRawPointer?) -> Void in
+                    // HACK: Swift doesn't allow C routine calls back to a generic closure
+                    // Use protocol Startable as a type eraser
+                    (Unmanaged<AnyObject>.fromOpaque(ptr!).takeRetainedValue() as! Startable).start()
+                },
+                Unmanaged.passUnretained(self).toOpaque(), stack.sptr, stack.ssze)
     }
-    
+
+    deinit {
+        coro_stack_free(&stack)
+    }
+}
+
+public class Coroutine<UpType, DownType>: Startable {
+    var stack: coro_stack
+    var caller_ctx: coro_context
+    var coro_ctx: coro_context
+    var entry: (DownType, _: (UpType) -> DownType) -> Void
+    var completed: Bool
+    var upValue: UpType?
+    var downValue: DownType?
+
+    func start() {
+        // Now we're in callee context
+        // Invoke entry with the first down value
+        self.entry(downValue!, { (up: UpType) -> DownType in
+            self.upValue = up
+            self.downValue = Optional.none
+            coro_transfer(&self.coro_ctx, &self.caller_ctx)
+            return downValue!
+        })
+        completed = true
+        // Switch back to caller
+        coro_transfer(&self.coro_ctx, &self.caller_ctx)
+    }
+
+    /**
+     * Called from caller context, continues the coroutine
+     * Returns an upValue set by yield called by coroutine
+     */
+    public func next(withValue: DownType) -> UpType? {
+        if (!completed) {
+            self.downValue = withValue
+            self.upValue = Optional.none
+            coro_transfer(&self.caller_ctx, &self.coro_ctx)
+        }
+        return self.upValue
+    }
+
+    /**
+     * Create a coroutine, supply a callback function acts as "yield"
+     * Coroutine calls the callback to yield an upValue
+     */
+    public init(entry: @escaping (DownType, _: (UpType) -> DownType) -> Void, withSuggestedStackSize: UInt32 = 0) {
+        stack = coro_stack()
+        coro_stack_alloc(&stack, withSuggestedStackSize)
+        caller_ctx = coro_context()
+        coro_ctx = coro_context()
+        self.entry = entry
+        completed = false
+        coro_create(&coro_ctx,
+                { (ptr: UnsafeMutableRawPointer?) -> Void in
+                    // HACK: Swift doesn't allow C routine calls back to a generic closure
+                    // Use protocol Startable as a type eraser
+                    (Unmanaged<AnyObject>.fromOpaque(ptr!).takeRetainedValue() as! Startable).start()
+                },
+                Unmanaged.passUnretained(self).toOpaque(), stack.sptr, stack.ssze)
+    }
+
     deinit {
         coro_stack_free(&stack)
     }
--- Tests/CoroutineTests/CoroutineTests.swift
+++ Tests/CoroutineTests/CoroutineTests.swift
@@ -1,34 +1,50 @@
 import XCTest
 @testable import Coroutine
 
-func c(yield: (Int)->()) {
+func c(yield: (Int) -> ()) {
     yield(1)
     yield(2)
     yield(3)
 }
 
+func c1(initValue: String, yield: (Int) -> String) {
+    XCTAssertEqual(initValue, "1")
+    XCTAssertEqual(yield(1), "2")
+    XCTAssertEqual(yield(2), "3")
+    XCTAssertEqual(yield(3), "")
+}
+
 class CoroutineTests: XCTestCase {
-    func testCoroutine() {
-        let cr = Coroutine<Int>(entry:c)
+    func testCoroutineSequence() {
+        let cr = CoroutineSequence(entry: c)
         XCTAssertEqual(cr.next(), 1)
         XCTAssertEqual(cr.next(), 2)
         XCTAssertEqual(cr.next(), 3)
         XCTAssertNil(cr.next())
     }
-    
-    func testIterator() {
-        var n=0;
-        let cr = Coroutine(entry:c)
+
+    func testCoroutineIterator() {
+        var n = 0;
+        let cr = CoroutineSequence(entry: c)
         for i in cr {
-            n=n+1
+            n = n + 1
             XCTAssertEqual(n, i)
         }
     }
 
-    static var allTests : [(String, (CoroutineTests) -> () throws -> Void)] {
+    func testCoroutine() {
+        let cr = Coroutine(entry: c1)
+        XCTAssertEqual(cr.next(withValue: "1"), 1)
+        XCTAssertEqual(cr.next(withValue: "2"), 2)
+        XCTAssertEqual(cr.next(withValue: "3"), 3)
+        XCTAssertNil(cr.next(withValue: ""))
+    }
+
+    static var allTests: [(String, (CoroutineTests) -> () throws -> Void)] {
         return [
-            ("testCoroutine", testCoroutine),
-            ("testIterator", testIterator),
+                ("testCoroutineSequence", testCoroutineSequence),
+                ("testCoroutineIterator", testCoroutineIterator),
+                ("testCoroutine", testCoroutine),
         ]
     }
 }
